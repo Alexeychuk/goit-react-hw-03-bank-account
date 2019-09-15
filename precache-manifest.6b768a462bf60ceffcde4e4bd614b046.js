b0461ecccabfdef9935fbb274591b20b135761df
Vladimir Alekseychuk <alekseychuk972mail.ru@gmail.com>
2019-09-15 13:18:07 +0300

    Updates

---
+++ precache-manifest.6b768a462bf60ceffcde4e4bd614b046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc12a62b14ec4d520401d9e041fb9e2f",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "32418d5c399d40f77087",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "c59b0ce16c66f4b565eb",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.45bcad73.chunk.css"
+  },
+  {
+    "revision": "32418d5c399d40f77087",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.ff6bdac1.chunk.js"
+  },
+  {
+    "revision": "c59b0ce16c66f4b565eb",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.86457f19.chunk.js"
+  },
+  {
+    "revision": "2ce760ce333fda087249",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.8add6ca6.js"
+  }
+]);
